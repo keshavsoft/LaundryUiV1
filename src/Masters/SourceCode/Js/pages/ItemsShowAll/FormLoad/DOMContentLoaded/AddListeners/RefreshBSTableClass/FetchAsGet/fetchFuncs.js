1dad1a567e289a4a54ca9964c1476bbc755ea880
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2025-09-26 11:56:25 +0530

    Masters New Endpoints

--- src/Masters/SourceCode/Js/pages/ItemsShowAll/FormLoad/DOMContentLoaded/AddListeners/RefreshBSTableClass/FetchAsGet/fetchFuncs.js
+++ src/Masters/SourceCode/Js/pages/ItemsShowAll/FormLoad/DOMContentLoaded/AddListeners/RefreshBSTableClass/FetchAsGet/fetchFuncs.js
@@ -1,7 +1,9 @@
 import getUrlJson from './getUrl.json' with {type: 'json'};
+import ConfigJson from "../../../../../../Config.json" with { type: "json" };
 
 let StartFunc = async () => {
-    let jVarLocalFetchUrl = getUrlJson.GetEndPoint;
+    let LocalroutePath = ConfigJson.routePath;
+    let jVarLocalFetchUrl = `/${LocalroutePath}/${getUrlJson.GetEndPoint}`;
 
     let response = await fetch(jVarLocalFetchUrl);
 
